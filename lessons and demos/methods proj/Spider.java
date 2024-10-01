b4ec948a149945a4a344980c558eafdbc01c9a08
ChaseW007 <chase.weinberger@rcsd.ca>
2024-10-01 09:44:01 -0600

    yes

---
+++ lessons and demos/methods proj/Spider.java
@@ -0,0 +1,17 @@
+public class Spider {
+    public void speak() {
+        System.out.println("Im a spider!");
+    }
+        
+    public void saydo() {
+        System.out.println("I like to spin webs");
+
+    }
+
+    public void iam() {
+        System.out.println("I am nice, i dont bite");
+
+    }
+
+} 
+
---
+++ lessons and demos/methods proj/SpiderWe.java
@@ -0,0 +1,12 @@
+public class SpiderWe {
+
+    public static void main(String[] args) throws Exception {
+        Spider web = new Spider();
+
+        web.speak();
+        web.saydo();
+        web.iam();
+
+    }
+
+}
