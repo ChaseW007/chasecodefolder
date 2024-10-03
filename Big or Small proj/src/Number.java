9bab43a404b4a4a3c8e1c1368d4974535e0646d0
ChaseW007 <chase.weinberger@rcsd.ca>
2024-10-03 09:37:20 -0600

    save

---
+++ Big or Small proj/src/Number.java
@@ -0,0 +1,8 @@
+
+public class Number {
+    public static void check( int a ) {
+
+        System.out.println(); 
+        
+    }
+}
---
+++ Big or Small proj/src/NumberRunner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumberRunner {
+
+    public static void main( String args[], int a ) throws Exception {
+
+        Number Number = new Number();
+       
+        if(a <= 50 ) {
+            System.out.println("You Failed")
+        
+        
+        else if
+
+
+    
+    }
+
+}
