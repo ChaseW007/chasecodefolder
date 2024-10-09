0aa1240e0d41a1aa48c98e8206617e3e354bd4a5
ChaseW007 <chase.weinberger@rcsd.ca>
2024-10-09 09:30:25 -0600

    sum up stuff

---
+++ A10SU/A10SUNew/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        SumUpStuff test = new SumUpStuff();
+
+        test.setNums(5, 7);
+        test.sum();
+        test.print();
+    
+    }
+}
---
+++ A10SU/A10SUNew/src/SumUpStuff.java
@@ -0,0 +1,23 @@
+
+public class SumUpStuff {
+
+    private double one, two, sum;
+
+    public void setNums(double num1, double num2)
+        {
+            one = num1;
+            two = num2;
+        }
+
+    public void sum( )
+        {
+            sum = one + two;
+        }
+
+    public void print( )
+        {
+            System.out.println("The answer is" + sum);
+        }
+
+
+}
