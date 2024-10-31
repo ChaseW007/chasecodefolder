5ebb22f34078dc440598e7777b6bd4e41c02eb22
ChaseW007 <chase.weinberger@rcsd.ca>
2024-10-31 08:40:03 -0600

    save finished code

--- Area of a triangle/src/App.java
+++ Area of a triangle/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        //taking input from user
+        System.out.println("Enter the length for the two sides");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        System.out.println("Enter the angle");
+        double c = input.nextDouble();
+
+        //calculating angle 
+        double angle = Math.toRadians(c);
+
+        //calculate area
+        double areaofTriangle = 0.5* (a*b) * Math.sin(angle);
+
+        System.out.println("Area of Traiangle is" + areaofTriangle);
+
         
     }
 }
