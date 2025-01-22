50d2cc72c04f287d8fd17a06b59664000818e207
ChaseW007 <chase.weinberger@rcsd.ca>
2025-01-22 10:13:23 -0600

    save cfd attempt

---
+++ CFD Practical Prob/src/Calc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Calc {
+    Scanner keys = new Scanner(System.in);
+    public void scores(int i) {
+        if (i > -1) {
+            
+            
+        }
+    }
+
+}
