637da08ced25635513d4b9bdb460f24ae959887d
ChaseW007 <chase.weinberger@rcsd.ca>
2025-01-22 19:50:17 -0600

    save

---
+++ new loop stats/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("input an int : ");
+        Scanner input = new Scanner(System.in);
+
+        int Number1 = input.nextInt();
+        System.out.println("input a second int : ");
+
+        Scanner Secondnum = new Scanner(System.in);
+        int Number2 = Secondnum.nextInt();
+
+        Stats stats = new Stats();
+        stats.Print(Number1, Number2);
+    }
+}
---
+++ new loop stats/src/Stats.java
@@ -0,0 +1,31 @@
+public class Stats {
+    public static int even(int Number1, int Number2) {
+        int evencount = 0;
+        for (int x = Number1; x <= Number2; x++)
+            if (x % 2 != 0){
+                evencount++;
+            }
+        return evencount;
+    }
+    public static int odd(int Number1, int Number2) {
+        int oddcount = 0;
+        for (int x = Number1; x <= Number2; x++)
+            if (x % 2 != 0){
+                oddcount++;
+            }
+        return oddcount;
+    }
+    public static int total(int Number1, int Number2) {
+        int total = 0;
+        for (int x = Number1; x <= Number2; x++){
+            total = total + x;
+        }
+        return total;
+    }
+    public static int Print(int Number1, int Number2) {
+        System.out.println("Number of evens: " + even(Number1, Number2));
+        System.out.println("Number of odds: " + odd(Number1, Number2));
+        System.out.println("Total" + total(Number1, Number2));
+            return Number2;
+    }
+}
